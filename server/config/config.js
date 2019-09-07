ab2588a51dd1a509cb0a3b762484e3d9169cc898
Ronald <rdhernandez0302@gmail.com>
2019-09-06 23:57:53 -0500

    Cmabios

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://root:s1ugzgA5ttUAv1zr@cluster0-z3tol.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
