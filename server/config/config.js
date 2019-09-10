bffe2767447ac163158652c9bbc7c573e17584a9
Ronald <rdhernandez0302@gmail.com>
2019-09-10 10:06:11 -0500

    Gogole Sign-In

--- server/config/config.js
+++ server/config/config.js
@@ -23,6 +23,11 @@ process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
 process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
 
 // ================
+// Google Client Id
+// ================
+process.env.CLIENT_ID = process.env.CLIENT_ID || '370874250224-6vgfrnjtmsb8rlpp4asbep5390tmu5eo.apps.googleusercontent.com';
+
+// ================
 // Base de datos
 // ================
 let urlDB;
--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@ require('./config/config');
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const path = require('path');
 
 app.use(require('./routes/index'));
 
+//Habilitar la carpeta public
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false }, (err, res) => {
     if (err) throw err;
